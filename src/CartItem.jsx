17437b3cb66deee92a71a80329c12cbc6209ba72
Marcelo <marcelo23.m@gmail.com>
2024-08-06 00:47:54 -0300

    abm items

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,10 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    
+    cart.items.forEach( item => {
+
+    });
   };
 
   const handleContinueShopping = (e) => {
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -246,6 +248,13 @@ function ProductList() {
         e.preventDefault();
         setShowCart(false);
     };
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, 
+         }));
+      };
     return (
         <div>
             <div className="navbar" style={styleObj}>
