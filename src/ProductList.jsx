b1d9956d5966e93360fb422a39c82eb480a126c7
Marcelo <marcelo23.m@gmail.com>
2024-08-06 02:20:59 -0300

    deploy

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -220,7 +220,7 @@ function ProductList() {
             ]
         }
     ];
-    
+
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -292,6 +292,7 @@ function ProductList() {
                                     <div className="product-card" key={plantIndex}>
                                         <img className="product-image" src={plant.image} alt={plant.name} />
                                         <div className="product-title">{plant.name}</div>
+                                        <div className="product-price">{plant.cost}</div>
                                         {/*Similarly like the above plant.name show other details like description and cost*/}
                                         <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                     </div>
